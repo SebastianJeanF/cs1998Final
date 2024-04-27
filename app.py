2806dd26599c7a9cc412b89244adcb2ddd34c56b
Rishi Shah <rrs244@cornell.edu>
2024-04-27 13:03:25 -0400

    Initial commit

---
+++ app.py
@@ -0,0 +1,25 @@
+import json
+from flask import Flask, request
+import dao
+from db import db
+
+# define db filename
+db_filename = "todo.db"
+app = Flask(__name__)
+
+# setup config
+app.config["SQLALCHEMY_DATABASE_URI"] = f"sqlite:///{db_filename}"
+app.config["SQLALCHEMY_TRACK_MODIFICATIONS"] = False
+app.config["SQLALCHEMY_ECHO"] = True
+
+# initialize app
+db.init_app(app)
+with app.app_context():
+    db.create_all()
+
+# generalized response formats
+def success_response(data, code=200):
+    return json.dumps({"success": True, "data": data}), code
+
+def failure_response(message, code=404):
+    return json.dumps({"success": False, "error": message}), code
