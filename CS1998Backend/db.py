e48b2dcd71579fa7c132546a21f7f8e929598131
Rishi Shah <rrs244@cornell.edu>
2024-04-30 14:27:27 -0400

    Finished all 4 tables, made some revisions to Deck & Card

--- CS1998Backend/db.py
+++ CS1998Backend/db.py
@@ -4,6 +4,9 @@ db = SQLAlchemy()
 
 # your classes here
 
+# Note: One-to-many relationship from users to decks, classes to decks, and decks to cards
+#Note: db.relationship for "one" scope, and foreign keys for "many" scope
+
 class Deck(db.Model):
     """
     Deck Model
@@ -11,13 +14,17 @@ class Deck(db.Model):
     __tablename__ = 'decks'
     id = db.Column(db.Integer, primary_key=True)
     title = db.Column(db.String, nullable=False)
-    cards = db.relationship('Card', cascade="delete")
+    cards = db.relationship('Card', cascade="delete") #relates deck to cards table
+    user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False) 
+    class_id = db.Column(db.Integer, db.ForeignKey('classes.id'), nullable=False)
     is_public = db.Column(db.Boolean, nullable=False)
     
     def serialize(self):
         return {
             'id': self.id,
             'title': self.title,
+            'user_id' : self.user_id,
+            'is_public': self.is_public,
             'cards': [card.serialize() for card in self.cards]
         }
 
@@ -30,8 +37,7 @@ class Card(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     question = db.Column(db.String, nullable=False)
     answer = db.Column(db.String, nullable=False)
-    deck_id = db.Column(db.Integer, db.ForeignKey('decks.id'), nullable=False)
-    deck = db.relationship('Deck')
+    deck_id = db.Column(db.Integer, db.ForeignKey('decks.id'), nullable=False) #specific foreign key, a column for deck_id indicates id of associated deck.
 
     def serialize(self):
         return {
@@ -41,3 +47,36 @@ class Card(db.Model):
             'answer': self.answer
         }
 
+class User(db.Model):
+    """
+    User Model
+    """
+    __tablename__ = 'users'
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String, nullable=False)
+    password = db.Column(db.String, nullable=False)
+    decks = db.relationship('Deck', cascade="delete")
+ 
+    def serialize(self):
+        return {
+            'id': self.id,
+            'username': self.username,
+            'password': self.password,
+            'decks': [deck.serialize() for deck in self.decks]
+        }
+    
+class Class(db.Model):
+    """
+    Class Model
+    """
+    __tablename__ = 'classes'
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String, nullable=False)
+    decks = db.relationship('Deck', cascade="delete")
+ 
+    def serialize(self):
+        return {
+            'id': self.id,
+            'title': self.title,
+            'decks': [deck.serialize() for deck in self.decks]
+        }
