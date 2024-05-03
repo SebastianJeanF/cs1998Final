82cbcdc8158db31aa31a1bf752000862cb48f4d2
Rishi Shah <rrs244@cornell.edu>
2024-05-02 21:24:53 -0400

    Check

--- backend/app.py
+++ backend/app.py
@@ -1,6 +1,7 @@
 import json
 from flask import Flask, request
-from db import db
+from db import db, User, Card, Deck, Class 
+from flask_sqlalchemy import SQLAlchemy
 
 # define db filename
 db_filename = "todo.db"
@@ -29,24 +30,60 @@ def failure_response(message, code=404):
 def hello_world():
     return "Hello, World!"
 
+
+"""
+Route to create the user. Returns error code 400 if no usernmae or password provided in 
+request body, otherwise makes the new User, commits it to the database, and
+returns a 201 success code along with the serialization.
+"""
 @app.route("/api/users/", methods=["POST"])
 def create_user():
-    # temporary response (Code below isn't correct implementation and just for
-    # testing purposes)
     body = json.loads(request.data)
-    username = body.get("username")
-    password = body.get("password")
-    if username is None or password is None:
-        return failure_response("Username and password required")
-    return success_response({"username": username, "password": password})
+    username = body['username']
+    #these checks are in case front-end doesn't handle clicking without filling a field.
+    if username is None:
+        return failure_response("No username provided", 400)
+    password = body['password']
+    if password is None:
+        return failure_response("No password provided", 400)
+    
+    new_user = User(username=username, password=password) #create new Course object with given code and name
+    db.session.add(new_user) #add object to sqlAlchemy session and commit to database. This is effectively one row in courses table
+    db.session.commit()
+    return success_response(new_user.serialize(), 201)
 
-@app.route("/api/users/", methods=["GET"])
+"""
+Returns serialization all users in User
+"""
+@app.route("/api/users/", method = ["POST"])
 def get_users():
-    # temporary response
-    return success_response({"username": "John Doe", "password": "password"})
+    #note query.all() returns every value in this query as a list. 
+    users = [c.serialize() for c in User.query.all()] 
+    return success_response(users)
 
 
+"""
+Check if user with provided username and password exists. If does, then returns serialized form in success response 200, else "no such user" message with 400 code.
+"""
+@app.route("/api/users/")
+def get_spec_user():
+    #assuming the data is being passed in through login fields
+    body = json.loads(request.data)
+    username = body['username']
+    #these checks are in case front-end doesn't handle clicking without filling a field.
+    if username is None:
+        return failure_response("No username provided", 400)
+    password = body['password']
+    if password is None:
+        return failure_response("No password provided", 400)
+    user_val = User.query.filter_by(username=username, password=password).first()
+    if user_val is None:
+         return failure_response("false", 400)
+    else:
+        return success_response("true", 200)
 
+
+    
 
 
 
